204f1814b624fd3ff84234414b488218a2e66c86
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-05-09 11:43:09 +0100

    integrated bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,15 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @articles = Article.all
   end
 
   def show
